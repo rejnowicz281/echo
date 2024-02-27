2207cbeef999834a3c2f2ee7a619efb88f333311
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-02-27 13:59:32 +0100

    Implement discover page

---
+++ actions/posts/read/get-discovery-posts.ts
@@ -0,0 +1,53 @@
+import { Post } from "@/types/posts";
+import actionError from "@/utils/actions/action-error";
+import { ActionResponse } from "@/utils/actions/action-response";
+import actionSuccess from "@/utils/actions/action-success";
+import shuffle from "@/utils/general/shuffle-array";
+import { createClient } from "@/utils/supabase/server";
+
+export type PostsActionResponse = ActionResponse & {
+    posts?: Post[];
+};
+
+const getDiscoveryPosts = async (): Promise<PostsActionResponse> => {
+    const actionName = "getDiscoveryPosts";
+
+    const supabase = createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) return actionError(actionName, { error: "You must be logged in" });
+
+    const { data: friendships, error: friendshipsError } = await supabase
+        .from("friendships")
+        .select("requester, recipient")
+        .or(`requester.eq.${user.id}, recipient.eq.${user.id})`)
+        .is("accepted", true)
+        .order("created_at", { ascending: false });
+
+    if (friendshipsError) return actionError(actionName, { error: friendshipsError.message });
+
+    const users = friendships.map((friendship) =>
+        friendship.requester === user.id ? friendship.recipient : friendship.requester
+    );
+
+    users.push(user.id);
+
+    // get posts of everyone except friends and current user
+    const { data: posts, error: postsError } = await supabase
+        .from("posts")
+        .select("*, creator:users(*)")
+        .not("creator", "in", `(${users.join(",")})`)
+        .is("parent_post", null);
+
+    if (postsError) return actionError(actionName, { error: postsError.message });
+
+    // shuffle posts
+    const shuffledPosts = shuffle(posts);
+
+    return actionSuccess(actionName, { posts: shuffledPosts });
+};
+
+export default getDiscoveryPosts;
--- app/(dashboard)/discover/page.tsx
+++ app/(dashboard)/discover/page.tsx
@@ -1,5 +1,42 @@
-const DiscoverPage = () => {
-    return <div>Work in progress</div>;
+import getDiscoveryPosts from "@/actions/posts/read/get-discovery-posts";
+import PostContainer from "@/components/posts/post-container";
+import Link from "next/link";
+
+const DiscoverPage = async () => {
+    const data = await getDiscoveryPosts();
+
+    const posts = data.posts;
+
+    if (data.error) return <h1>Error loading posts: {data.error}</h1>;
+    if (!posts) return <h1>Couldn't load posts</h1>;
+
+    return (
+        <div className="p-12 mx-auto max-w-[800px] w-full flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
+                <h1 className="text-4xl font-bold">Discover</h1>
+                <div className="text-gray-500 flex flex-col gap-1">
+                    <p>Welcome to the discovery page! Here you can see posts of users you aren't friends with.</p>
+
+                    {posts.length <= 0 && (
+                        <p>
+                            It looks like there are no posts to show here... Click{" "}
+                            <Link
+                                className="
+                            text-blue-500 hover:underline
+                            "
+                                href="/"
+                            >
+                                here
+                            </Link>{" "}
+                            to visit your feed instead.
+                        </p>
+                    )}
+                </div>
+            </div>
+
+            {posts.length > 0 && posts.map((post) => <PostContainer key={post.id} post={post} />)}
+        </div>
+    );
 };
 
 export default DiscoverPage;
