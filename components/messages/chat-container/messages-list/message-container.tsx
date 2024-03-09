ea2fc119c44b9d83b8270db27333bb495d284728
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-03-09 15:08:22 +0100

    Add tooltips on chat page

--- components/messages/chat-container/messages-list/message-container.tsx
+++ components/messages/chat-container/messages-list/message-container.tsx
@@ -1,9 +1,11 @@
 import deleteMessage from "@/actions/messages/modify/delete-message";
 import PresenceAvatar from "@/components/general/presence-avatar";
 import SubmitButton from "@/components/general/submit-button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/shadcn/ui/tooltip";
 import useAuthContext from "@/providers/auth-provider";
 import { Message } from "@/types/message";
 import userDisplayName from "@/utils/general/user-display-name";
+import formatMessageDate from "@/utils/messages/format-message-date";
 import { AiOutlineLoading } from "@react-icons/all-files/ai/AiOutlineLoading";
 import { RiDeleteBinLine } from "@react-icons/all-files/ri/RiDeleteBinLine";
 import clsx from "clsx";
@@ -40,21 +42,35 @@ const MessageContainer: FC<MessageContainerProps> = ({ message }) => {
     return (
         <div className={clsx("group flex p-5 gap-4 flex-wrap", isSender ? "flex-row-reverse" : "flex-row")}>
             <div className="flex items-center justify-center">
-                <PresenceAvatar
-                    avatarSize={50}
-                    src={sender.avatar_url}
-                    alt={senderDisplayName}
-                    userId={message.sender}
-                />
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger className="cursor-default">
+                            <PresenceAvatar
+                                avatarSize={50}
+                                src={sender.avatar_url}
+                                alt={senderDisplayName}
+                                userId={message.sender}
+                            />
+                        </TooltipTrigger>
+                        <TooltipContent>{senderDisplayName}</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             </div>
-            <div
-                className={clsx(
-                    "p-4 rounded max-w-[500px]",
-                    isSender ? "bg-gray-800 text-white" : "bg-gray-200 text-black"
-                )}
-            >
-                {message.text}
-            </div>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <div
+                            className={clsx(
+                                "p-4 rounded max-w-[500px]",
+                                isSender ? "bg-gray-800 text-white" : "bg-gray-200 text-black"
+                            )}
+                        >
+                            {message.text}
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent>{formatMessageDate(message.created_at)}</TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
             <div className="self-center">
                 {message.loading ? (
                     <AiOutlineLoading className="animate-spin" />
